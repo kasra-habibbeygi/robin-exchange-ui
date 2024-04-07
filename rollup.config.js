10fc9b81fbcc0b6d8fb0a170d8aadd9366d03b99
unknown <kasrahabibbeygi@gmail.com>
2024-04-07 15:44:17 +0330

    fix: update packages

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-minification';
 
-const packageJson = require('./package.json');
+import packageJson from './package.json' assert { type: 'json' };
 
 export default [
     {
