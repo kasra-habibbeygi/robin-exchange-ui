567bdef8adb365bb39779bcd83afab931cef51d9
unknown <kasrahabibbeygi@gmail.com>
2024-04-07 14:18:15 +0330

    test husky

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,33 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+/* eslint-disable @typescript-eslint/no-var-requires */
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
-	{
-	  input: "src/index.ts",
-	  output: [
-		{
-		  file: packageJson.main,
-		  format: "cjs",
-		  sourcemap: true,
-		  exports: 'named',
-		},
-		{
-		  file: packageJson.module,
-		  format: "esm",
-		  sourcemap: true,
-		  exports: 'named',
-		},
-	  ],
-	  plugins: [
-		resolve(),
-		commonjs(),
-		typescript({ tsconfig: "./tsconfig.json" }),
-	  ],
-	},
-	{
-	  input: "src/index.ts",
-	  output: [{ file: "dist/index.d.ts", format: "es" }],
-	  plugins: [dts()],
-	},
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: packageJson.main,
+                format: 'cjs',
+                sourcemap: true,
+                exports: 'named'
+            },
+            {
+                file: packageJson.module,
+                format: 'esm',
+                sourcemap: true,
+                exports: 'named'
+            }
+        ],
+        plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
+    },
+    {
+        input: 'src/index.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts()]
+    }
 ];
