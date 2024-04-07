f8d1eedbd3354b95f2634f93bfff48bc68ecca82
unknown <kasrahabibbeygi@gmail.com>
2024-04-07 15:03:49 +0330

    feat: add terser

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-minification';
 
 const packageJson = require('./package.json');
 
@@ -23,7 +24,7 @@ export default [
                 exports: 'named'
             }
         ],
-        plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
+        plugins: [terser(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
     },
     {
         input: 'src/index.ts',
