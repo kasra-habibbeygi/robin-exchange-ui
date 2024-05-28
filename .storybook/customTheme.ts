c6dfa8ecd20f9b259053de7a48af50cfac7e9183
unknown <kasrahabibbeygi@gmail.com>
2024-05-28 15:19:22 -0600

    feat: change logo

---
+++ .storybook/customTheme.ts
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+import Logo from './logo.png'
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Your Project Title',
+  brandUrl: 'https://adakpro.com/',
+  brandImage: Logo,
+});
---
+++ src/core/accordion/accordion.tsx
@@ -0,0 +1,8 @@
+// Assets
+import { AccordionContainer } from './accordion.style';
+
+const Accordion = () => {
+    return <AccordionContainer>Accordion</AccordionContainer>;
+};
+
+export default Accordion;
