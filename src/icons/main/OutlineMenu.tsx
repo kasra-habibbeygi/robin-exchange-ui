3755071cea8721de57f52fe2156026cd4f36684d
unknown <kasrahabibbeygi@gmail.com>
2024-07-09 15:25:45 +0330

    fix: change menu icon

--- src/icons/main/OutlineMenu.tsx
+++ src/icons/main/OutlineMenu.tsx
@@ -2,10 +2,14 @@
 
 import type { SVGProps } from 'react';
 const SvgOutlineMenu = (props: SVGProps<SVGSVGElement>) => (
-    <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 24 24' {...props}>
-        <rect width={24} height={2} y={11} rx={1} />
-        <rect width={24} height={2} y={4} rx={1} />
-        <rect width={24} height={2} y={18} rx={1} />
+    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' {...props}>
+        <path
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth={1.5}
+            d='M5.498 12h13.005M5.498 16.002h13.005M5.498 7.998h13.005'
+        />
     </svg>
 );
 export default SvgOutlineMenu;
